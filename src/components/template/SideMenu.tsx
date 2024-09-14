2ff77e0787bfd47227bd5c3501cc580bb463d51d
Renan <renanclaude.rcg@gmail.com>
2024-09-14 16:12:11 -0300

    Creating a Logo

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,14 +1,20 @@
 import { bellIcon, homeIcon, settingsIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function SideMenu() {
   return (
     <aside>
+      <div className={`h-20 w-20
+        bg-gradient-to-r from-indigo-500 to-purple-800
+        flex flex-col items-center justify-center`}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" text="Início" icon={homeIcon} />
-        <MenuItem url="/configuracoes" text="Ajustes" icon={settingsIcon} />
+        <MenuItem url="/ajustes" text="Ajustes" icon={settingsIcon} />
         <MenuItem url="/notificacoes" text="Notificações" icon={bellIcon} />
-
       </ul>
     </aside>
   )
--- src/pages/ajustes.tsx
+++ src/pages/ajustes.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/template/Layout";
 
 export default function Settings() {
   return (
-    <Layout title="Configurações" subtitle="Personalize o sistema aqui!">
+    <Layout title="Ajustes" subtitle="Personalize o sistema aqui!">
     <h3>Conteúdo!</h3>
   </Layout>
   )
