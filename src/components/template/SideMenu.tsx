facd1adcd709f191291347bb425823c35ab0107b
Renan <renanclaude.rcg@gmail.com>
2024-09-14 02:46:43 -0300

    Improving SideMenu + creating /notificacoes and /configuracoes

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,7 +1,15 @@
+import { bellIcon, homeIcon, settingsIcon } from "../icons";
+import MenuItem from "./MenuItem";
+
 export default function SideMenu() {
   return (
-   <aside>
-    <h1>Menu</h1>
-   </aside>
+    <aside>
+      <ul>
+        <MenuItem url="/" text="Início" icon={homeIcon} />
+        <MenuItem url="/configuracoes" text="Ajustes" icon={settingsIcon} />
+        <MenuItem url="/notificacoes" text="Notificações" icon={bellIcon} />
+
+      </ul>
+    </aside>
   )
 }
---
+++ src/pages/configuracoes.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/template/Layout";
+
+export default function Settings() {
+  return (
+    <Layout title="Configurações" subtitle="Personalize o sistema aqui!">
+    <h3>Conteúdo!</h3>
+  </Layout>
+  )
+}
