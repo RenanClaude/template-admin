f67ab2b16b544437d72e47e593d6f76f5a53581c
Renan <renanclaude.rcg@gmail.com>
2024-09-19 05:39:03 -0300

    styling the authentication page

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,9 +9,17 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return props.doNotRender ? null : (
-    <div className={`flex flex-col`}>
+    <div className={`flex flex-col mt-4`}>
+
       <label>{props.label}</label>
-      <input type={props.type ?? "text"} value={props.value} onChange={e => props.changeValue(e.target.value)} required={props.mandatory} />
+
+      <input
+        type={props.type ?? "text"}
+        value={props.value}
+        onChange={e => props.changeValue(e.target.value)}
+        required={props.mandatory}
+        className={`px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:outline-none focus:bg-white`}
+      />
     </div>
   )
 }
--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -6,13 +6,37 @@ export default function Authentication() {
   const [password, setPassword] = useState("");
   const [mode, setMode] = useState<"login" | "register">("login");
 
-
+  function toSubmit() {
+    if (mode === "login") {
+      console.log("login");
+    } else {
+      console.log("cadastro")
+    }
+  }
 
   return (
-    <div>
-      <h1>Autenticação</h1>
-      <AuthInput label="Email" value={value} type="email" changeValue={setValue} mandatory />
-      <AuthInput label="Senha" value={password} type="password" changeValue={setPassword} mandatory />
+    <div className={`flex flex-col h-screen items-center justify-center`}>
+
+      <div className={`w-1/2`}>
+
+        <h1 className={`text-xl font-bold mb-5`}>
+          {mode === "login" ? "Entre com a sua conta" : "Cadastre-se na plataforma"}
+        </h1>
+
+        <AuthInput label="Email" value={value} type="email" changeValue={setValue} mandatory />
+        <AuthInput label="Senha" value={password} type="password" changeValue={setPassword} mandatory />
+
+        <button onClick={toSubmit} className={`w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6`}>
+          {mode === "login" ? "Entrar" : "Cadastrar"}
+        </button>
+
+        <hr className={`my-6 border-gray-300 w-full`} />
+
+        <button onClick={toSubmit} className={`w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3 `}>
+          Entrar com o Google
+        </button>
+
+      </div>
 
     </div>
   )
